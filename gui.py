8aa41e96bf580d2ef27193fa0976c271af165160
Shahabuddin14 <shahabuddin650@gmail.com>
2026-05-09 22:43:18 +0600

    Edit function added

--- gui.py
+++ gui.py
@@ -4,10 +4,17 @@ import FreeSimpleGUI as Fsg
 label = Fsg.Text("Type in a to do")
 input_box = Fsg.InputText(tooltip="Enter to do", key="todo")
 add_button = Fsg.Button("Add")
+list_box =  Fsg.Listbox(values=sf.get_todos(),
+                        key="todos",
+                        enable_events=True,
+                        size=[45, 10]
+                        )
+edit_button = Fsg.Button("Edit")
 
 window = Fsg.Window('To-Do-App',
-                    layout=[[label],[input_box, add_button]],
-                    font=("Helvetica", 12)
+                    layout=[[label],[input_box, add_button],
+                            [list_box, edit_button]],
+                    font=("Helvetica", 20)
                     )
 while True:
     event, values = window.read()
@@ -19,6 +26,20 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             sf.write_todos(todos)
-        case Fsg.Win_Close:
+            window["todos"].Update(values=todos)
+
+        case "Edit":
+            edit_todo = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+
+            todos = sf.get_todos()
+            index = todos.index(edit_todo)
+            todos[index] = new_todo
+            sf.write_todos(todos)
+            window["todos"].Update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+        case Fsg.WIN_CLOSED:
             break
 window.close()
